d87507c3e249fa429ba60191917aac4dfea76d2c
AlecDannmayr <alec.dannmayr@gmail.com>
2023-05-17 10:20:25 +1000

    tags

---
+++ udemy/app/public/wp-content/themes/udemy/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset=<?php bloginfo('charset'); ?>>
+    <?php wp_head(); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Udemy</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <p>Udemy</p>
+    <?php wp_footer() ?>
+</body>
+
+</html>
